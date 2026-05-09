999b42b6fd187f4f096e911ab81e7ca8275fdd34
meats88 <pcx883mi@gmail.com>
2026-05-09 14:43:25 -0700

    donenne

--- main.hpp
+++ main.hpp
@@ -11,10 +11,21 @@ int getClosestMed(int, int, int);
 
 int getRandom(void)
 {
-    // TODO
+    return rand() % 100;
 }
 
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
-    // TODO
+    double mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
+    double diff1 = abs(rdnum1 - mean);
+    double diff2 = abs(rdnum2 - mean);
+    double diff3 = abs(rdnum3 - mean);  
+
+    if(diff1 <= diff2 && diff1 <= diff3){
+        return rdnum1;
+    } else if(diff2 <= diff1 && diff2 <= diff3){
+        return rdnum2;
+    } else {
+        return rdnum3;
+    }
 }
